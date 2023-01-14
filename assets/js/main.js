27574a7b152ebf6bbb989b89df8038e65308b4d0
= <=>
2023-01-14 15:22:30 +0530

    FIREBASE

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+import { initializeApp } from 'firebase/app';
+
 /*==================== MENU SHOW Y HIDDEN ====================*/
 const navMenu=document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
